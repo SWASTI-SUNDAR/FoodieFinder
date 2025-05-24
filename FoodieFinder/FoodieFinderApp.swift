cb8f68619685a53449531604dad8c0cc0868e33a
Swasti Sundar Pradhan <swastipradhan044@gmail.com>
2025-05-24 20:38:20 +0530

    Initial Commit

---
+++ FoodieFinder/FoodieFinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodieFinderApp.swift
+//  FoodieFinder
+//
+//  Created by Swasti Sundar Pradhan on 24/05/25.
+//
+
+import SwiftUI
+
+@main
+struct FoodieFinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FoodieFinderTests/FoodieFinderTests.swift
@@ -0,0 +1,17 @@
+//
+//  FoodieFinderTests.swift
+//  FoodieFinderTests
+//
+//  Created by Swasti Sundar Pradhan on 24/05/25.
+//
+
+import Testing
+@testable import FoodieFinder
+
+struct FoodieFinderTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
